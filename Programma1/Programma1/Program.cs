5a7f2992c6c6f3470d87a81785f608ed47dac0ba
Maxouwe <100154435+Maxouwe@users.noreply.github.com>
2024-05-15 19:31:51 +0200

    prog1 klaar

--- Programma1/Programma1/Program.cs
+++ Programma1/Programma1/Program.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
+using System.Data.SqlClient;
 using System.Data.SQLite;
 using System.IO;
+using System.Runtime.CompilerServices;
 
 namespace Programma1
 {
     internal class Program
     {
         static string metaConnectionString = @"Data Source=..\..\..\db\meta.db;Version=3";
-        static string intermediatesConnectionString = @"Data Source=..\..\..\db\intermediates.db;Version=3";
         static int numTuples;
+
+        //delegate function type that we can use as callback function for reading tuples from a table
+        public delegate void readFunc(SQLiteDataReader reader);
 
         static void Main(string[] args)
         {
@@ -21,71 +25,53 @@ namespace Programma1
             //create the database files
             SQLiteConnection.CreateFile(@"..\..\..\db\meta.db");
 
-            //make the connections
+            //make a connection with the database
             SQLiteConnection metaConnection = new SQLiteConnection(metaConnectionString);
             metaConnection.Open();
 
+            //put autompg table into the metadb
+            executeSQL(metaConnection, File.ReadAllText(@"..\..\..\db\autompg.sql"));
 
-            //create intermediateresults table
-            createIntermediateTables(metaConnection);
-            
-            //retrieve the amount of tuples in autompg
-            using (SQLiteCommand command = new SQLiteCommand(metaConnection))
-            {
-                command.CommandText = @"SELECT COUNT(*) AS c FROM autompg";
-                using (SQLiteDataReader reader = command.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        numTuples = reader.GetInt32(reader.GetOrdinal("c"));
-                    }
+            //count the amount of tuples in autompg
+            readTuples(metaConnection, 
+                @"SELECT COUNT(*) AS amountOfTuples FROM autompg", 
+                delegate (SQLiteDataReader reader) 
+                { 
+                    numTuples = reader.GetInt32(reader.GetOrdinal("amountOfTuples")); 
                 }
-            }
+                );
+            
+            //put all qf and idf tables in the metadb
+            executeSQL(metaConnection, File.ReadAllText(@"..\..\..\db\metadb.txt"));
 
-            //put categorical IDF in the intermediate tables
+            //put categorical IDF in the idf tables
             collectCategoricalIDF(metaConnection);
             
-            //dictionary with the QFs of all attribute values
+            //calculate all QF values from the workload
             //numerical QF are still handled as categorical QFs
             collectQF(metaConnection);
 
             //retrieve the qf and idf from their tables and fill the qfidf tables inside the metadb
-            using (SQLiteCommand command = new SQLiteCommand(metaConnection))
-            {
-                command.CommandText = File.ReadAllText(@"..\..\..\db\metaload.txt");
-                command.ExecuteNonQuery();
-            }
-            
-           
+            executeSQL(metaConnection, File.ReadAllText(@"..\..\..\db\metaload.txt"));
 
-            using (SQLiteCommand command = new SQLiteCommand(metaConnection))
-            {
-                //now we do the same but with a select statement
-                command.CommandText = @"SELECT * FROM brandqfidf";
+            //use qf and idf for categorical attributes
+            //use qf for exact numerical values
+            //calculate idf at query time
 
-                //make a reader that will read the db and retrieve the tuples we asked for
-                //de using statment zorgt dat zodra we uit de scope van reader zijn, de reader niet meer actief is
-                using (SQLiteDataReader reader = command.ExecuteReader())
+            readTuples(metaConnection,
+                @"SELECT * FROM displacementqf",
+                delegate (SQLiteDataReader reader)
                 {
-                    while (reader.Read())
-                    {
-                        Console.WriteLine(reader.GetString(reader.GetOrdinal("brand")) + reader.GetFloat(reader.GetOrdinal("qfidf")));
-                    }
-                }
-            }
+                    Console.WriteLine(reader.GetFloat(reader.GetOrdinal("displacement")) + "=" + reader.GetFloat(reader.GetOrdinal("qf")));
+                });
         }
 
-        //verwijdert alle tables die gebruikt zijn voor intermediate results
-        static void deleteIntermediateTables(SQLiteConnection connection)
-        {
-
-        }
         //finds all QFs and puts them in the table specified through the connection parameter
         static void collectQF(SQLiteConnection connection)
         {
             string[] lines = File.ReadLines(@"..\..\..\db\workload.txt").ToArray();
 
-            int RQFMax = 0;
+            float RQFMax = 0;
 
             //make dictionary so we can look up currently known RQF of an attribute by the attribute name
             Dictionary<string, float> RQFs = new Dictionary<string, float>();
@@ -126,71 +112,106 @@ namespace Programma1
                 }
             }
 
-            //now we know all RQFs and RQFMax so we can compute the QFs
-            //we will replace the RQFs by their QFs
-            //for now the numerical QF is handled like categorical QF, change later
-            foreach(KeyValuePair<string, float> pair in RQFs)
-            {
-                RQFs[pair.Key] = pair.Value / RQFMax; 
-            }
-            //remember despite the dictionary being called RQFs it now contains all the QFs instead
 
-            //insert them into the QF tables
-            using (SQLiteCommand command = new SQLiteCommand(connection))
-            {
-                foreach (KeyValuePair<string, float> pair in RQFs)
-                {
-                    
-                    string attribute = pair.Key.Split('=')[0];
-                    string attributeValue = pair.Key.Split('=')[1];
-                    command.CommandText = String.Format(@"INSERT INTO {0}qf VALUES({1}, {2})", attribute, "\'" + attributeValue + "\'", pair.Value);
-                    command.ExecuteNonQuery();
-                }
-            }
-
+            //fill all qf tables
+            fillQFTable(connection, "brand", RQFs, RQFMax);
+            fillQFTable(connection, "model", RQFs, RQFMax);
+            fillQFTable(connection, "type", RQFs, RQFMax);
+            fillQFTable(connection, "mpg", RQFs, RQFMax);
+            fillQFTable(connection, "cylinders", RQFs, RQFMax);
+            fillQFTable(connection, "displacement", RQFs, RQFMax);
+            fillQFTable(connection, "horsepower", RQFs, RQFMax);
+            fillQFTable(connection, "weight", RQFs, RQFMax);
+            fillQFTable(connection, "acceleration", RQFs, RQFMax);
+            fillQFTable(connection, "model_year", RQFs, RQFMax);
+            fillQFTable(connection, "origin", RQFs, RQFMax);
 
         }
- 
+
+        //fill QF table for attribute based on the RQF and RQFMax
+        static void fillQFTable(SQLiteConnection connection, string attribute, Dictionary<string, float> RQFs, float RQFMax)
+        {
+            readTuples(connection,
+                String.Format(@"SELECT {0} FROM autompg GROUP BY {0}", attribute),
+                delegate (SQLiteDataReader reader)
+                {
+                    string val;
+                    try
+                    {
+                        val = reader.GetString(reader.GetOrdinal(attribute));
+                    }
+                    catch
+                    {
+                        val = reader.GetFloat(reader.GetOrdinal(attribute)).ToString();
+                    }
+                    string key = attribute + "=" + val;
+                    if (RQFs.ContainsKey(key))
+                    {
+                        executeSQL(connection, String.Format(@"INSERT INTO {0}qf VALUES({1}, {2})", attribute, "\'" + val + "\'", (RQFs[key] + 1)/(RQFMax + 1)));
+                    }
+                    else
+                    {
+                        executeSQL(connection, String.Format(@"INSERT INTO {0}qf VALUES({1}, {2})", attribute, "\'" + val + "\'", 1 / (RQFMax + 1)));
+                    }
+                }
+                );
+        }
+        
+        //calc standard deviation for an attribute
+        static float calcStandardDeviation(SQLiteConnection connection, string attribute)
+        {
+            float sigma = 0;
+            readTuples(connection,
+                String.Format(@"SELECT STDEV({0}) AS stdev FROM autompg", attribute),
+                delegate (SQLiteDataReader reader)
+                {
+                    sigma = reader.GetFloat(reader.GetOrdinal("stdev"));
+                });
+            return sigma;
+        }
+
         static void collectCategoricalIDF(SQLiteConnection connection)
         {
 
             //collect idf for the brand attribute
-            string sqlStatements = String.Format(@"INSERT INTO brandidf SELECT brand, LOG({0}/COUNT(brand)) FROM autompg GROUP BY brand", numTuples);
-            using (SQLiteCommand command = new SQLiteCommand(connection))
-            {
-                command.CommandText = sqlStatements;
-                command.ExecuteNonQuery();
-            }
+            executeSQL(connection, String.Format(@"INSERT INTO brandidf SELECT brand, LOG({0}/COUNT(brand)) FROM autompg GROUP BY brand", numTuples));
 
             //collect idf for the model attribute
-            sqlStatements = String.Format(@"INSERT INTO modelidf SELECT model, LOG({0}/COUNT(model)) FROM autompg GROUP BY model", numTuples);
-            using (SQLiteCommand command = new SQLiteCommand(connection))
-            {
-                command.CommandText = sqlStatements;
-                command.ExecuteNonQuery();
-            }
+            executeSQL(connection, String.Format(@"INSERT INTO modelidf SELECT model, LOG({0}/COUNT(model)) FROM autompg GROUP BY model", numTuples));
 
             //collect idf for the type attribute
-            sqlStatements = String.Format(@"INSERT INTO typeidf SELECT type, LOG({0}/COUNT(type)) FROM autompg GROUP BY type", numTuples);
-            using (SQLiteCommand command = new SQLiteCommand(connection))
+            executeSQL(connection, String.Format(@"INSERT INTO typeidf SELECT type, LOG({0}/COUNT(type)) FROM autompg GROUP BY type", numTuples));
+
+        }
+
+
+        //execute the sql statements from given by the string
+        //using the db file signified by the dbConnection
+        static void executeSQL(SQLiteConnection dbConnection, string sqlStatements)
+        {
+            using (SQLiteCommand command = new SQLiteCommand(dbConnection))
             {
                 command.CommandText = sqlStatements;
                 command.ExecuteNonQuery();
             }
         }
 
-        static void createIntermediateTables(SQLiteConnection connection)
+        //reads tuples from a database 
+        //sqlStatement should be a SELECT statement
+        //use the function f to decide what to do with each tuple
+        static void readTuples(SQLiteConnection dbConnection, string sqlStatement, readFunc f)
         {
-            using (SQLiteCommand command = new SQLiteCommand(connection))
+            using (SQLiteCommand command = new SQLiteCommand(dbConnection))
             {
-                //create the autompg table
-                string sqlStatements = File.ReadAllText(@"..\..\..\db\autompg.sql");
-                command.CommandText = sqlStatements;
-                command.ExecuteNonQuery();
-
-                //create the intermediate results tables
-                command.CommandText = File.ReadAllText(@"..\..\..\db\intermediates.txt");
-                command.ExecuteNonQuery();
+                command.CommandText = sqlStatement;
+                using (SQLiteDataReader reader = command.ExecuteReader())
+                {
+                    //for each found tuple perform the callback function f
+                    while (reader.Read())
+                    {
+                        f(reader);
+                    }
+                }
             }
         }
     }
